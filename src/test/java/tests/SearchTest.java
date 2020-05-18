b87676e48d552534d4a9b0b251d8c809dca8d9e4
Brijesh <brijesh_shivnani@outlook.com>
2020-05-18 18:27:27 -0400

    Added Desription to tests and removed unused package

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -21,18 +20,21 @@ public class SearchTest extends TestBase {
 		searchPage = new SearchPage();
 	}
 
+//	Test 1.	Search for apples and sort the search results from highest price to lowest price.
 	@Test(priority=1,enabled=true)
 	   public void productSearch() throws InterruptedException{
 		searchPage.searchProduct();
 		Assert.assertTrue(searchPage.verifySearchIsSorted());;
 	   }
 	
+//	Test 2.	Search for apples and use the Price Reduction option under Filter By > Promotion to sort the search by sale badges. 
 	@Test(priority=2,enabled=true)
 	public void verifyCart() throws Exception {
 		searchPage.searchProduct();
 		Assert.assertTrue(searchPage.verifyItemInCart());
 	}
 	
+//	Test 3.	As a new user to the site, add an item to your cart
 	@Test(priority=3,enabled=true)
 	public void verifyFilterPromotion() throws Exception {
 		searchPage.searchProduct();
@@ -40,6 +42,9 @@ public class SearchTest extends TestBase {
 		Assert.assertTrue(searchPage.verifyPriceReductionMatch());
 	}
 	
+//	Test 4.	From the home page, click on “Change Location” or “Choose a location” if selecting location 
+//	first time, search for “QUEEN STREET WEST” and select it for pickup, Go to location details
+//	by clicking the link present on same page.
 	@Test(priority=4,enabled=true)
 	public void verifyLocation() throws Exception {
 //		login is not working on LoBlaws site through automation
